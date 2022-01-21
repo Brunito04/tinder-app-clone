c5482241b2d4050f47f7efd2b71993975c2821ec
Brunito04 <bruno2004b@gmail.com>
2022-01-21 18:50:33 -0300

    Inital setup of the React app

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="App">
+      {/* Header */}
+      <Header /> 
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+function header() {
+  return (
+    <div className='header'>
+        <h2>I'm a header</h2>
+    </div>
+  );
+}
+
+export default header;
